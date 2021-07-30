67a689b3fc81f2dc73b16e1d1fa84dbc968d5597
Maic Miller <maicmiller42@gmail.com>
2021-07-30 20:20:34 -0300

    :sparkles: feat(api staties):  Get staties
    implementation of the api to search the states

---
+++ cities-api/src/main/java/com/gitbub/maicmiller/citiesapi/staties/State.java
@@ -0,0 +1,72 @@
+package com.gitbub.maicmiller.citiesapi.staties;
+
+import com.gitbub.maicmiller.citiesapi.countries.Country;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class State {
+    @Entity(name = "State")
+    @Table(name = "estado")
+    @TypeDefs({
+            @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+    })
+    public static class State {
+
+        @Id
+        private Long id;
+
+        @Column(name = "nome")
+        private String name;
+
+        private String uf;
+
+        private Integer ibge;
+
+        @Column(name = "pais")
+        private Integer countryId;
+
+        @ManyToOne
+        @JoinColumn(name = "pais", referencedColumnName = "id")
+        private Country country;
+
+        @Type(type = "jsonb")
+        @Basic(fetch = FetchType.LAZY)
+        @Column(name = "ddd", columnDefinition = "jsonb")
+        private List<Integer> ddd;
+
+        public State() {
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getUf() {
+            return uf;
+        }
+
+        public Integer getIbge() {
+            return ibge;
+        }
+
+        public List<Integer> getDdd() {
+            return ddd;
+        }
+
+        public Country getCountry() {
+            return country;
+        }
+
+       public Integer getCountryId() {
+          return countryId;
+       }
+    }
+}
---
+++ cities-api/src/main/java/com/gitbub/maicmiller/citiesapi/staties/StateResource.java
@@ -0,0 +1,25 @@
+package com.gitbub.maicmiller.citiesapi.staties;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+public class StateResource {
+    @RestController
+    @RequestMapping("/staties")
+    public class StateResource {
+
+        private final StateRepository repository;
+
+        public StateResource(final StateRepository repository) {
+            this.repository = repository;
+        }
+
+        @GetMapping
+        public List<State> staties() {
+            return repository.findAll();
+        }
+    }
+}
